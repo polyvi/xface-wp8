790c773066f0348635fea7179889a849366dadcc
Yangtb <yangtb@polyvi.com>
2014-01-14 13:34:26 +0800

    add ToastPrompt

---
+++ xFaceLib/xFaceLib/toast/XToastPrompt.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using Coding4Fun.Toolkit.Controls;
+
+namespace xFaceLib.toast
+{
+    class XToastPrompt
+    {
+        private static XToastPrompt instance;
+
+        public static XToastPrompt GetInstance()
+        {
+            if (null == instance)
+            {
+                instance = new XToastPrompt();
+            }
+            return instance;
+        }
+
+        public void Toast(string content, string title = "")
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                ToastPrompt toast = new ToastPrompt();
+                toast.Title = title;
+                toast.Message = content;
+                toast.Show();
+            });
+        }
+    }
+}
